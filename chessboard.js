4bd70c16c80baa725cdd3911dad8e2c7a4c2199a
WaltRibeiro <walt@fororchestra.com>
2019-12-17 20:21:19 -0500

    first commit

---
+++ chessboard.js
@@ -0,0 +1,24 @@
+let size = parseInt(process.argv.slice(2));
+const chessboard = (boardsize) => {
+let whitesqr = " ";
+let blacksqr = "#";
+let newline = "\n";
+let board = "";
+let toggle = true;
+for (let y = 1 ; y <= boardsize ; y++){
+if (y % 2 === 0){toggle = false;} else {toggle = true;}
+for (let x = 1; x <= boardsize; x++){
+if (toggle == true){
+board = board + whitesqr;
+toggle = false;
+} else {
+board = board + blacksqr;
+toggle = true;
+}
+}
+board = board + newline;
+}
+console.log(board);
+}
+
+chessboard(size);
